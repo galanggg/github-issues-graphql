4f54cb5f10d5f4344477abb56b3123d4dab68e6a
Galanggg <tugasgalang@gmail.com>
2020-01-17 13:33:25 +0800

    Normal Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test Graphql Tracking issues</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
