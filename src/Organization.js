07a124c137012f85b948b23b4055fd50a4fbe4fe
Galanggg <tugasgalang@gmail.com>
2020-01-18 11:06:47 +0800

    add props for button star & unstar

--- src/Organization.js
+++ src/Organization.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Repository from './Repository'
 
-const Organization = ({organization, errors, onFetchMoreIssues}) => {
+const Organization = ({organization, errors, onFetchMoreIssues, onStarRepository}) => {
   if (errors) {
     return (
       <p>error</p>
@@ -14,7 +14,7 @@ const Organization = ({organization, errors, onFetchMoreIssues}) => {
       <strong>Issue from Organization : </strong>
       <a href={organization.url}>{organization.name}</a>
     </p>
-    <Repository repository={organization.repository} onFetchMoreIssues={onFetchMoreIssues} />
+    <Repository repository={organization.repository} onFetchMoreIssues={onFetchMoreIssues} onStarRepository={onStarRepository} />
   </div>
   )
 }
--- src/Repository.js
+++ src/Repository.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Repository = ({repository, onFetchMoreIssues}) => (
+const Repository = ({repository, onFetchMoreIssues, onStarRepository}) => (
   <div>
     <p>
       <strong>In Repository:</strong>
       <a href={repository.url}>{repository.name}</a>
     </p>
+    <button type="button" onClick={() => onStarRepository(repository.id, repository.viewerHasStarred)}>
+      {repository.viewerHasStarred ? 'Unstar' : 'Star'}
+    </button>
     <ul>
       {repository.issues.edges.map(issue => (
         <li key={issue.node.id}>
